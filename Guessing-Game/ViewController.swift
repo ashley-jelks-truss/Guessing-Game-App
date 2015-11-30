46b42f241635a78a82197f08459b9de764b12b07
Ashley Jelks <ashley.jelks.truss@Ashleys-MacBook-Pro.local>
2015-11-30 15:46:01 -0800

    first commit

--- Guessing-Game/ViewController.swift
+++ Guessing-Game/ViewController.swift
@@ -9,7 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var userGuess: UITextField!
+    
+    @IBOutlet var randomlyGeneratedNumberLabel: UILabel!
+    
+    @IBAction func submitUserGuess(sender: AnyObject) {
+    
+        let randomlyGeneratedNumber = String(arc4random_uniform(11))
+        
+        print(randomlyGeneratedNumber)
+        
+        if randomlyGeneratedNumber == userGuess.text {
+            
+            randomlyGeneratedNumberLabel.text = "Your Guess Was Correct!!"
+            
+        }
+        
+        else {
+        
+        randomlyGeneratedNumberLabel.text = "Sorry, The Answer Was " + randomlyGeneratedNumber + " !"
+        }
+        
+        
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
